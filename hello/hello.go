5441179a872813537bfb019facb4c439a885f5e9
TableStar <afraroad@gmail.com>
2024-09-25 02:34:28 +0700

    adding subtests to hello_test.go and refactoring hello.go to accomodate new tests

--- hello/hello.go
+++ hello/hello.go
@@ -6,6 +6,9 @@ const engHelloPrefix = "Hello, "
 
 func Hello(name string) string {
 	// return fmt.Sprintf("Hello, %s", name)
+	if name == "" {
+		name = "World"
+	}
 	return engHelloPrefix + name
 }
 func main() {
