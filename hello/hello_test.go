fd08398de484721ec0edfe333285684e0c2c544c
TableStar <afraroad@gmail.com>
2024-09-25 02:48:40 +0700

    refactor: adding helper func to hello_test.go and refactoring the subtests

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -6,17 +6,18 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMsg(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMsg(t, got, want)
 	})
+}
+
+func assertCorrectMsg(t testing.TB, got, want string) {
+	t.Helper() // tells the testing framework that this func is helper function
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
