940832193e5cf8352004ab54d64c46aaa3e86d8e
TableStar <afraroad@gmail.com>
2024-09-25 03:28:15 +0700

    feat: adding spanish,french, and german option and its related tests

--- hello/hello.go
+++ hello/hello.go
@@ -1,16 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
+const spanish = "spanish"
+const french = "french"
+const german = "german"
 const engHelloPrefix = "Hello, "
+const spaHelloPrefix = "Hola, "
+const fraHelloPrefix = "Bonjour, "
+const gerHelloPrefix = "Hallo, "
 
-func Hello(name string) string {
+func Hello(name, lang string) string {
 	// return fmt.Sprintf("Hello, %s", name)
 	if name == "" {
 		name = "World"
 	}
-	return engHelloPrefix + name
+	prefix := engHelloPrefix
+
+	switch strings.ToLower(lang) {
+	case spanish:
+		prefix = spaHelloPrefix
+	case french:
+		prefix = fraHelloPrefix
+	case german:
+		prefix = gerHelloPrefix
+	}
+	// if strings.ToLower(lang) == spanish {
+	// 	return spaHelloPrefix + name
+	// }
+	// if strings.ToLower(lang) == french {
+	// 	return fraHelloPrefix + name
+	// }
+	return prefix + name
 }
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
