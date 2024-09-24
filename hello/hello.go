9faf62c273bb0268258ccebd5a2f27ce9be20573
TableStar <afraroad@gmail.com>
2024-09-25 02:09:55 +0700

    refactor: refactoring hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
+const engHelloPrefix = "Hello, "
+
 func Hello(name string) string {
 	// return fmt.Sprintf("Hello, %s", name)
-	return "Hello, " + name
+	return engHelloPrefix + name
 }
 func main() {
 	fmt.Println(Hello("world"))
