08fa1b0dda7069922cb3d69cc8994941ccc5b492
TableStar <afraroad@gmail.com>
2024-09-27 21:22:13 +0700

    feat: refactor repeat and add Example and Benchmark for repeat

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,10 +1,9 @@
 package iteration
 
-const repeatCount = 5
-
-func Repeat(character string) string {
+// Repeat takes a string & an integer and return the string repeated by the value of the integer
+func Repeat(character string, repeat int) string {
 	var repeated string
-	for i := 0; i < repeatCount; i++ {
+	for i := 0; i < repeat; i++ {
 		repeated += character
 	}
 	return repeated
