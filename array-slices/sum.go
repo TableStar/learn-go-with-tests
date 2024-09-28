c86d5f31f051c4abdfc929fa18287bd4fa6a66b8
TableStar <afraroad@gmail.com>
2024-09-28 20:16:23 +0700

    feat: add SumAllTails and its test

--- array-slices/sum.go
+++ array-slices/sum.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
@@ -15,6 +17,21 @@ func SumAll(numbersToSum ...[]int) []int {
 
 	for _, numbers := range numbersToSum {
 		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+			fmt.Println(sums)
+		} else {
+			tails := numbers[1:]
+			sums = append(sums, Sum(tails))
+		}
 	}
 	return sums
 }
