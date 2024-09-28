27c2044f67eedf9f69b8198a282e1462a82bf31f
TableStar <afraroad@gmail.com>
2024-09-28 16:38:33 +0700

    feat:add func SumAll and its tests

--- array-slices/sum_test.go
+++ array-slices/sum_test.go
@@ -1,15 +1,31 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"golang.org/x/exp/slices"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	// numbers := [...]int{1, 2, 3, 4, 5}
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 6
 
-	if got != want {
-		t.Errorf("got %d want %d given %v", got, want, numbers)
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	//guide used reflect.DeepEqual but go v1.21 has slice standard package which has slices.Equal function to do a simple shallow compare on slices
+	//Note that this function expects the elements to be comparable. So, it can't be applied to slices with non-comparable elements like 2D slices.
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
