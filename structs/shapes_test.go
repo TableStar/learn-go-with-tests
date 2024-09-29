5999c664d4f6a330d98efb15d64ccec11b973fd0
TableStar <afraroad@gmail.com>
2024-09-30 01:09:26 +0700

    refactor: clearing up areaTests slice of structs and error statements

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -18,37 +17,23 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 32.0},
 	}
-
-	fmt.Println(areaTests)
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		want := tt.want
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			want := tt.want
+			if got != want {
+				t.Errorf("%#v got %g want %g", tt.name, got, want)
+			}
+		})
 	}
-
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
-	}
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
 }
