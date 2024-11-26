d5b0c17647f2c7618a6259d6731605a90914a028
TableStar <afraroad@gmail.com>
2024-11-26 21:43:45 +0700

    feat: added logic if arabic is 4 or 5

--- roman-numeral/numeral.go
+++ roman-numeral/numeral.go
@@ -1,5 +1,20 @@
 package roman_numeral
 
+import "strings"
+
 func ConvertToRoman(arabic int) string {
-	return "I"
+	var res strings.Builder
+
+	for i := arabic; i > 0; i-- {
+		if i == 5 {
+			res.WriteString("V")
+			break
+		}
+		if i == 4 {
+			res.WriteString("IV")
+			break
+		}
+		res.WriteString("I")
+	}
+	return res.String()
 }
